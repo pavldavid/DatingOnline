48a3f8445435e8f97aba4bae9fff416cf1d7bd29
David Pavlenko <Dpavlenko@mail.greenriver.edu>
2021-05-27 01:04:17 -0400

    completed the php page and also the view pages

--- index.php
+++ index.php
@@ -7,81 +7,158 @@ error_reporting(E_ALL);
 
 //Require autoload file
 require_once('vendor/autoload.php');
+require_once ('model/data-layer.php');
+require_once ('model/validation.php');
 
 //start a session
 session_start();
 
 //Instantiate Fat-Free and controller
 $f3 = Base::instance();
-$con = new Controller($f3);
 
-//Define a default route
+//default route
 $f3->route('GET /', function () {
-    $GLOBALS['con']->home();
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
-
-$f3->route('GET|POST /personal', function () {
-    $GLOBALS['controller']->personal();
-});
-
-$f3->route('GET|POST /profile', function () {
-    $GLOBALS['controller']->profile();
-});
-
-$f3->route('GET|POST /interest', function () {
-    $GLOBALS['controller']->interest();
-});
-
-$f3->route('GET /summary', function () {
-    $GLOBALS['controller']->summary();
-});
-
 
 //redirect to the personal info page
-/**
- * $f3->route('GET|POST /personalInfo', function () {
 
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $_SESSION['fName'] = $_POST['fName'];
-        $_SESSION['lName'] = $_POST['lName'];
-        $_SESSION['age'] = $_POST['age'];
-        $_SESSION['gender'] = $_POST['gender'];
-        $_SESSION['phoneNum'] = $_POST['phoneNum'];
-        header('location: profile');
+  $f3->route('GET|POST /info', function ($f3) {
+
+      $_SESSION = array();
+      $userInfo = array();
+
+      $infoFirst = "";
+      $infoLast = "";
+      $infoAge = "";
+      $infoNum = "";
+      $infoGen = "";
+
+
+      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $infoFirst = $_POST['fname'];
+        $infoLast = $_POST['lname'];
+        $infoAge = $_POST['age'];
+        $infoGen = $_POST['gender'];
+        $infoNum = $_POST['phoneNum'];
+
+
+        if(validName($infoFirst))
+        {
+            $_SESSION['fName'] = $infoFirst;
+        }else
+        {
+            $f3->set('errors[fname]', 'Please enter a valid First Name');
+        }
+
+        if (validName($infoLast))
+        {
+            $_SESSION['lname'] = $infoLast;
+        }else
+        {
+            $f3->set('errors[lname]', 'Please enter a valid Last Name');
+        }
+
+        if (validAge($infoAge))
+        {
+            $_SESSION['age'] = $infoAge;
+        } else {
+            $f3->set('errors["age"]', "Please enter a valid age");
+        }
+
+          if (validPhone($infoNum)) {
+              $_SESSION['phoneNum'] = $infoNum;
+          } else {
+              $f3->set('errors["number"]', "Please enter a valid phone number");
+          }
+          $f3->set('infoFirst', $infoFirst);
+          $f3->set('infoLast', $infoLast);
+          $f3->set('infoAge', $infoAge);
+          $f3->set('infoNum', $infoNum);
+          $f3->set('infoGen', $infoGen);
+          if (empty($f3->get('errors'))) {
+              header('location: profile');
+          }
     }
 
     //we display the information page
     $view = new Template();
-    echo $view->render('views/personalInfo.html');
+    echo $view->render('views/info.html');
 });
 
 //next redirect to the profile.html page
-$f3->route('GET|POST /profile', function () {
+$f3->route('GET|POST /profile', function ($f3) {
+    $profileEmail = "";
+    $profileState = "";
+    $profileSeeking = "";
+    $profileBio = "";
+
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         //var_dump($_POST);
-        $_SESSION['email'] = $_POST['email'];
-        $_SESSION['state'] = $_POST['state'];
-        $_SESSION['seeking'] = $_POST['seeking'];
-        $_SESSION['bio'] = $_POST['bio'];
+        $profileEmail = $_POST['email'];
+        $profileState = $_POST['state'];
+        $profileSeeking = $_POST['seeking'];
+        $profileBio = $_POST['bio'];
 
-        header('location: interest');
+        if (validEmail($profileEmail)){
+            $_SESSION['email'] = $profileEmail;
+        }else {
+            $f3->set('errors["email"]', "Please enter a valid email address");
+        }
+
+        $_SESSION['seeking'] = $profileSeeking;
+        $_SESSION['state'] = $profileState;
+        $_SESSION['bio'] = $profileBio;
+
+        $f3->set('profileEmail', $profileEmail);
+        $f3->set('profileState', $profileState);
+        $f3->set('profileSeeking', $profileSeeking);
+        $f3->set('profileBio', $profileBio);
+
+
+        if (empty($f3->get('errors'))) {
+            header('location: interest');
+        }
     }
-
+    $f3->set('states', getStates());
     //we display the profile.html page
     $view = new Template();
     echo $view->render('views/profile.html');
 });
 
-$f3->route('GET|POST /interest', function () {
+$f3->route('GET|POST /interest', function ($f3) {
+    $interestIndoor = array();
+    $interestOutdoor = array();
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        //var_dump($_POST);
-        $_SESSION['in_door'] = implode(" ", $_POST['in_door']);
-        $_SESSION['out_door'] = implode(" ", $_POST['out_door']);
 
-        header('location: sumary');
+        $interestIndoor = $_POST['in_door'];
+        $interestOutdoor = $_POST['out_door'];
+
+        var_dump($_POST);
+        if(validIndoor($interestIndoor)){
+            $_SESSION['indoor'] = implode(" ", $interestIndoor);
+        }else {
+            $f3->set('errors["indoor"]', 'Please enter a valid interest');
+        }
+        if (validOutdoor($interestOutdoor)) {
+            $_SESSION['outdoor'] = implode(", ", $interestOutdoor);
+        } else {
+            $f3->set('errors["outdoor"]', 'Please enter a valid interest');
+        }
+        if (empty($f3->get('errors'))) {
+            header('location: sumary');
+        }
+
     }
+    $f3->set('indoor', getIndoor());
+    $f3->set('outdoor', getOutdoor());
+
+    $f3->set('interestIndoor', $interestIndoor);
+    $f3->set('interestOutdoor', $interestOutdoor);
+
     //we display the interests page
     $view = new Template();
     echo $view->render('views/interest.html');
@@ -90,8 +167,8 @@ $f3->route('GET|POST /interest', function () {
 $f3->route('GET /sumary', function () {
     //show the summary page
     $view = new Template();
-    echo $view->render('views/sumary.html');
-});*/
+    echo $view->render('views/summary.html');
+});
 
 //Run Fat-Free
 $f3->run();
